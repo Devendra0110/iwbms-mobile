a1efe46e92ab85327360d5bdfd422f72772f0923
Bhargav bodige <darsbogi@gmail.com>
2019-11-22 17:10:57 +0530

    edu 4 form done

--- src/app/claim-management/claim-forms/educational-forms/claim-education4/claim-education4.page.ts
+++ src/app/claim-management/claim-forms/educational-forms/claim-education4/claim-education4.page.ts
@@ -1,4 +1,7 @@
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+import { ClaimValidationService } from './../../../../services/claim-validation.service';
 
 @Component({
   selector: 'app-claim-education4',
@@ -7,9 +10,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClaimEducation4Page implements OnInit {
 
-  constructor() { }
+  public formGroup: FormGroup;
+
+  constructor(private validationService: ClaimValidationService) {
+
+    this.formGroup = new FormGroup({
+      // english form controls
+      familyRelation: new FormControl('', this.validationService.createValidatorsArray('familyRelation')),
+      aadharNumber: new FormControl('', this.validationService.createValidatorsArray('aadharNumber')),
+      schoolIdDoc: new FormControl('', this.validationService.createValidatorsArray('schoolIdDoc')),
+      college: new FormControl('', this.validationService.createValidatorsArray('college')),
+      placeCollege: new FormControl('', this.validationService.createValidatorsArray('placeCollege')),
+      dateOfAdmission: new FormControl('', this.validationService.createValidatorsArray('dateOfAdmission')),
+      certificates: new FormControl('', this.validationService.createValidatorsArray('certificates')),
+      receipt: new FormControl('', this.validationService.createValidatorsArray('receipt')),
+      // declaration: new FormControl('', this.validationService.createValidatorsArray('declaration')),
+      standard: new FormControl('', this.validationService.createValidatorsArray('standard')),
+      selfDeclaration: new FormControl('', this.validationService.createValidatorsArray('selfDeclaration')),
+      rationCardDoc: new FormControl('', this.validationService.createValidatorsArray('rationCardDoc')),
+      bookReceipt: new FormControl('', this.validationService.createValidatorsArray('bookReceipt')),
+      aadharCardDoc: new FormControl('', this.validationService.createValidatorsArray('aadharCardDoc')),
+      bonafideDoc: new FormControl('', this.validationService.createValidatorsArray('bonafideDoc')),
+      degreeName: new FormControl('', this.validationService.createValidatorsArray('degreeName')),
+      insEmail: new FormControl('', this.validationService.createValidatorsArray('insEmail')),
+      insPhNo: new FormControl('', this.validationService.createValidatorsArray('insPhNo')),
+      degreeName_mr: new FormControl('', this.validationService.createValidatorsArray('degreeName_mr')),
+      age: new FormControl('', this.validationService.createValidatorsArray('age')),
+      benefitType: new FormControl('', this.validationService.createValidatorsArray('benefitType')),
+      benefitAmount: new FormControl(''),
+      category: new FormControl('', this.validationService.createValidatorsArray('category')),
+      verifyDocumentCheck: new FormControl('', this.validationService.createValidatorsArray('verifyDocumentCheck')),
+
+      // marathi form controls
+      placeCollege_mr: new FormControl(''),
+      college_mr: new FormControl(''),
+    });
+
+   }
 
   ngOnInit() {
   }
 
+  //marathi getters
+  get college_mr(): AbstractControl { return this.formGroup.get('college_mr'); }
+  get degreeName_mr(): AbstractControl { return this.formGroup.get('degreeName_mr'); }
+  get placeCollege_mr(): AbstractControl { return this.formGroup.get('placeCollege_mr'); }
+
+  //english getters
+  get verifyDocumentCheck() { return this.formGroup.get('verifyDocumentCheck'); }
+  get familyRelation() { return this.formGroup.get('familyRelation'); }
+  get college() { return this.formGroup.get('college'); }
+  get aadharNumber() { return this.formGroup.get('aadharNumber'); }
+  get schoolIdDoc() { return this.formGroup.get('schoolIdDoc'); }
+  get standard() { return this.formGroup.get('standard'); }
+  get placeCollege() { return this.formGroup.get('placeCollege'); }
+  get dateOfAdmission() { return this.formGroup.get('dateOfAdmission'); }
+  get certificates() { return this.formGroup.get('certificates'); }
+  get receipt() { return this.formGroup.get('receipt'); }
+  get aadharCardDoc() { return this.formGroup.get('aadharCardDoc'); }
+  get insEmail() { return this.formGroup.get('insEmail'); }
+  get insPhNo() { return this.formGroup.get('insPhNo'); }
+  get age() { return this.formGroup.get('age'); }
+  get bonafideDoc() { return this.formGroup.get('bonafideDoc'); }
+  get category() { return this.formGroup.get('category'); }
+  get degreeName() { return this.formGroup.get('degreeName'); }
+  get rationCardDoc() { return this.formGroup.get('rationCardDoc'); }
+  get bookReceipt() { return this.formGroup.get('bookReceipt'); }
+  // get declaration() { return this.formGroup.get('declaration'); }
+  get selfDeclaration() { return this.formGroup.get('selfDeclaration'); }
 }
