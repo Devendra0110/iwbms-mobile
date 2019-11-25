7649347e15e26619030cd16a594b28717bfa449f
flash <sachinkotian007@yahoo.com>
2019-11-25 10:51:09 +0530

    error fix in f4

--- src/app/claim-management/claim-forms/financial-forms/claim-financial4/claim-financial4.page.ts
+++ src/app/claim-management/claim-forms/financial-forms/claim-financial4/claim-financial4.page.ts
@@ -1,30 +1,50 @@
+import { Storage } from '@ionic/storage';
+import { HttpService } from './../../../../services/http.service';
+import { TransliterationService } from './../../../../services/transliteration.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormControlDirective, FormGroup, Validators } from '@angular/forms';
 import { ClaimValidationService } from 'src/app/services/claim-validation.service';
+import { ClaimBasePage } from 'src/app/claim-management/claim-base/claim-form.baseclass';
+import { ClaimService } from 'src/app/services/claim.service';
+import { Router } from '@angular/router';
+import { Toast } from '@ionic-native/toast/ngx';
+import { Dialogs } from '@ionic-native/dialogs/ngx';
 
 @Component({
   selector: 'app-claim-financial4',
   templateUrl: './claim-financial4.page.html',
   styleUrls: ['./claim-financial4.page.scss'],
 })
-export class ClaimFinancial4Page implements OnInit {
+export class ClaimFinancial4Page extends ClaimBasePage implements OnInit {
   public formGroup: FormGroup;
-  public maxTodaysDate : string;
+  public maxTodaysDate: string;
+  public todaysDate: any;
 
-  constructor(private validationService: ClaimValidationService) {
-  this.formGroup = new FormGroup({
+  constructor(private validationService: ClaimValidationService,
+    protected transliterate: TransliterationService,
+    protected httpService: HttpService,
+    protected claimService: ClaimService,
+    protected router: Router,
+    protected storage: Storage,
+    protected toast: Toast,
+    private dialogs: Dialogs,
+) {
 
-    pmAwaasCertificate: new FormControl('', this.validationService.createValidatorsArray('pmAwaasCertificate')),
-    // declaration: new FormControl('', this.validationService.createValidatorsArray('declaration')),
-    selfDeclaration: new FormControl('', this.validationService.createValidatorsArray('selfDeclaration')),
-    verifyDocumentCheck: new FormControl('', this.validationService.createValidatorsArray('verifyDocumentCheck')),
-    placeOfClaim: new FormControl('', Validators.required)
+    super(transliterate, httpService, claimService, router, storage, toast);
 
-    //marathi form controls
-    ,
-    benefitType: new FormControl('', [Validators.required]),
-    benefitAmount: new FormControl('')
-  });
+    this.formGroup = new FormGroup({
+
+      pmAwaasCertificate: new FormControl('', this.validationService.createValidatorsArray('pmAwaasCertificate')),
+      // declaration: new FormControl('', this.validationService.createValidatorsArray('declaration')),
+      selfDeclaration: new FormControl('', this.validationService.createValidatorsArray('selfDeclaration')),
+      verifyDocumentCheck: new FormControl('', this.validationService.createValidatorsArray('verifyDocumentCheck')),
+      placeOfClaim: new FormControl('', Validators.required)
+
+      //marathi form controls
+      ,
+      benefitType: new FormControl('', [Validators.required]),
+      benefitAmount: new FormControl('')
+    });
   }
 
   ngOnInit() {
