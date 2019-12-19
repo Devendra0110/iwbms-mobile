ab1e2cd14ee632496c53f9d48ee63d9717c143e0
Bhargav bodige <darsbogi@gmail.com>
2019-12-19 12:20:00 +0530

    etc changes

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,5 +1,5 @@
-export const serverUrl = 'http://192.168.1.24:3000/api/';
+export const serverUrl = 'http://192.168.1.108:3000/api/';
 // http://192.168.1.177:3000/api/
 // http://localhost:3000/api/;
 // https://iwbms.mbocww.in/api/
-export const storageServerUrl = 'http://192.168.1.24:3002/'
+export const storageServerUrl = 'http://192.168.1.108:3002/'
