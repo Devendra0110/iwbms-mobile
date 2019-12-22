a3942326f00ef1b644e830b3ce35a7ee8e6e4286
Devendra <devendragaud01@gmail.com>
2019-12-22 18:51:22 +0530

    ip changed to server

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,5 +1,5 @@
-export const serverUrl = 'http://localhost:3000/api/';
+export const serverUrl = 'https://iwbms.mahabocw.in/api/';
 // http://192.168.1.177:3000/api/
 // http://localhost:3000/api/;
-// https://iwbms.mbocww.in/api/
-export const storageServerUrl = 'http://localhost:3002/'
+// https://iwbms.mahabocw.in/api/
+export const storageServerUrl = 'https://storage.mahabocw.in/'
